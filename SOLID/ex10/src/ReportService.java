3061b7a31cbf64001c987b63baf323ecf1405b24
deysandip301 <deysandip301@gmail.com>
2025-08-24 21:34:53 +0530

    refactor: create report service and Logger repository

--- SOLID/ex10/src/ReportService.java
+++ SOLID/ex10/src/ReportService.java
@@ -1,6 +1,11 @@
 public class ReportService {
+    private ILogger logger;
+
+    public ReportService(ILogger logger) {
+        this.logger = logger;
+    }
+
     void generate(){
-        ConsoleLogger logger = new ConsoleLogger();
         logger.log("Generating daily report...");
         System.out.println("Report contents...");
     }
