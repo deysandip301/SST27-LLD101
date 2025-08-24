e0b66fb0afd866a67fc2b9fed766292b4509200f
deysandip301 <deysandip301@gmail.com>
2025-08-24 21:28:16 +0530

    refactor: create interface for order repository

--- SOLID/ex09/src/Demo09.java
+++ SOLID/ex09/src/Demo09.java
@@ -1,5 +1,5 @@
 public class Demo09 {
     public static void main(String[] args) {
-        new OrderController().create("ORD-1");
+        new OrderController(new SqlOrderRepository()).create("ORD-1");
     }
 }
